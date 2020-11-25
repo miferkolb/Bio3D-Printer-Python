d284218e6371a0bb57f5a0306e1c0af7a82c76a1
Andres <ansanz@usal.es>
2020-11-25 20:53:30 +0100

    mods 2

--- BioWitbox4Heads.py
+++ BioWitbox4Heads.py
@@ -21,73 +21,100 @@ class BioWitboxModification(Script):
             "version": 1,
             "settings":
             {
-                "searchM104":
+                "search_M10_49_":
                 {
-                    "label": "Search Set hotend temperature",
+                    "label": "Search Set hotend temperature and Wait for hotend temperature",
                     "description": "All occurrences of this text will get replaced by the replacement text.",
                     "type": "str",
-                    "default_value": "M104 S([0-9]+) T([0-9]+); set extruder temperature"
+                    "default_value": "^M10[49] S([0-9]+) T([0-9]+)"
                 },
-                "replaceM104":
+                "replace_M10_49_":
                 {
                     "label": "Replace",
                     "description": "The search text will get replaced by this text.",
                     "type": "str",
-                    "default_value": "; deleted M104"
+                    "default_value": "; deleted M10[49]"
                 },
-                "is_regexM104":
+                "is_regex_M_10_49_":
                 {
                     "label": "Use Regular Expressions",
                     "description": "When enabled, the search text will be interpreted as a regular expression.",
                     "type": "bool",
-                    "default_value": false
-                }
-                "searchM109":
+                    "default_value": true
+                },
+
+                "search_Tx":
                 {
-                    "label": "Search Wait for hotend temperature",
+                    "label": "Search Set hotend temperature and Wait for hotend temperature",
                     "description": "All occurrences of this text will get replaced by the replacement text.",
                     "type": "str",
-                    "default_value": "M109 S([0-9]+) T([0-9]+); wait for temperature to be reached"
+                    "default_value": "^T([0-9.]+)"
                 },
-                "replaceM109":
+                "replace_Tx":
                 {
                     "label": "Replace",
                     "description": "The search text will get replaced by this text.",
                     "type": "str",
-                    "default_value": "; deleted M109"
+                    "default_value": "; Lets change the tool"
                 },
-                "is_regexM109":
+                "is_regex_Tx":
                 {
                     "label": "Use Regular Expressions",
                     "description": "When enabled, the search text will be interpreted as a regular expression.",
                     "type": "bool",
-                    "default_value": false
+                    "default_value": true
                 }
+
             }
         }"""
 
     def execute(self, data):
-        search_string = self.getSettingValueByKey("searchM104")
-        if not self.getSettingValueByKey("is_regexM104"):
+        
+        # Search Set hotend temperature M104 and Wait for hotend temperature M109
+        search_string = self.getSettingValueByKey("search_M10_49_")
+        if not self.getSettingValueByKey("is_regex_M_10_49_"):
             search_string = re.escape(search_string) #Need to search for the actual string, not as a regex.
         search_regex = re.compile(search_string)
 
-        replace_string = self.getSettingValueByKey("replaceM104")
+        replace_string = self.getSettingValueByKey("replace_M10_49_")
 
         for layer_number, layer in enumerate(data):
             data[layer_number] = re.sub(search_regex, replace_string, layer) #Replace all.
+        
+        ################################################
+
+        # Search
+
+ 
+
+        search_string = self.getSettingValueByKey("search_Tx")
+        if not self.getSettingValueByKey("is_regex_Tx"):
+            search_string = re.escape(search_string) #Need to search for the actual string, not as a regex.
+        search_regex = re.compile(search_string)
+
+        for layer_number, layer in enumerate(data):
+            if re.search(search_regex, layer):
+                tool = re.search(search_regex, layer)
+                num_tool = tool.group(1)
+            
+            
+            if num_tool < 1:
+                print(num_tool)
+            else if num_tool 
+
+
+                #   data[layer_number] = re.sub(search_regex, replace_string, layer) #Replace all.
+
+        
+
+
+
+        # replace_string = self.getSettingValueByKey("replace_Tx")
+
+        
+            data[layer_number] = re.sub(search_regex, replace_string, layer) #Replace all.
+
+
+
 
         return data
-
-    def execute(self, data):
-        search_string = self.getSettingValueByKey("searchM109")
-        if not self.getSettingValueByKey("is_regexM109"):
-            search_string = re.escape(search_string) #Need to search for the actual string, not as a regex.
-        search_regex = re.compile(search_string)
-
-        replace_string = self.getSettingValueByKey("replaceM109")
-
-        for layer_number, layer in enumerate(data):
-            data[layer_number] = re.sub(search_regex, replace_string, layer) #Replace all.
-
-        return data        
